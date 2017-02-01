0d35f6d4117c30edc0d544ec28652940139a1d1c
Kacper Mucha <kacper.mucha@predica.pl>
2017-02-01 15:23:06 +0100

    added Get-AzureRmVmImageSetting script

---
+++ Get-AzureRmVmImageSetting.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    Gets list of all possible Azure VM image Publishers, Offers and Skus
+.EXAMPLE
+    PS C:\> Get-AzureRmVmImageSetting.ps1 -Location "West Europe"
+    Gets Azure VM image settings for location West Europe
+.NOTES
+    Small helper script that assumes that you are already logged in using Login-AzureRmAccount.
+    Omitted error handling on purpose.
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    $Location
+)
+
+process {
+    $publishers = (Get-AzureRmVMImagePublisher -Location $location).PublisherName
+    
+    $offers = $publishers | ForEach-Object {
+        $publisher = $_
+        Write-Verbose -Message "Processing publisher: $publisher"
+        Get-AzureRmVMImageOffer -PublisherName $publisher -Location $location
+    }    
+    
+    $skus = $offers | ForEach-Object {
+        $offer = $_
+        Write-Verbose -Message "Processing offer: $($offer.Offer)"
+        Get-AzureRmVMImageSku -PublisherName $offer.PublisherName -Offer $offer.Offer -Location $location
+    }
+
+    $outputProperties = @{
+        Publishers = $publishers
+        Offers = $offers.Offer
+        Skus = $skus.Skus
+    }
+    $output = New-Object -TypeName psobject -Property $outputProperties
+       
+    $output        
+}
