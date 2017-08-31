00a729d017981c288d8676769aa1a756fb184488
Kacper Mucha <kacper.mucha@gmail.com>
2017-08-31 23:53:10 +0200

    add Convert-ExchangeSizeToByte function

--- Helpers.ps1
+++ Helpers.ps1
@@ -5,3 +5,30 @@ Register-ArgumentCompleter -ParameterName 'SubscriptionName' -ScriptBlock {
         [System.Management.Automation.CompletionResult]::new("`"$($_.Name)`"")
     }
 }
+
+function Convert-ExchangeSizeToByte {
+    <#
+    .SYNOPSIS
+        Converts string data returned by Exchange cmdlets to bytes
+    .EXAMPLE
+        PS C:\> "{0:N2}" -f ((Convert-ExchangeSizeToByte -InputObject "10.64 MB (11,158,319 bytes)") / 1MB)
+        Converts string in Exchange cmdlet format to MB value with two decimal places
+    .INPUTS
+        System.String
+    .OUTPUTS
+        System.Int
+    #>
+    [CmdletBinding()]
+    param (
+        $InputObject
+    )
+
+    begin {
+        $Regex = "\((.+)\)"
+    }
+
+    process {
+        $Bytes = [Regex]::Match($InputObject, $Regex).Groups[1].Value.Replace(" bytes", "").Replace(",", "")
+        [int]$Bytes
+    }
+}
